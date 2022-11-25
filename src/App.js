87144be999d55d01fbd851002e43b4f14832d0e0
Kelvin Kamau <kelvinkam24@gmail.com>
2022-11-25 14:37:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState} from 'react'
+import { images } from './images';
 
-function App() {
+const App = () => {
+ const[index,setIndex]= useState(0);
+ const {id,job,image,name} = images[index];
+
+ const checkNumber = (number)=>{
+  if(number > images.length -1){
+   return 0;
+  }
+  if(number < 0){
+   return images.length -1;
+  }
+  return number;
+ }
+
+ const nextPerson = ()=>{
+  setIndex((index)=>{
+   let newIndex = index + 1;
+   return checkNumber(newIndex);
+  })
+ }
+
+ const prevPerson = ()=>{
+  setIndex((index)=>{
+   let newIndex = index - 1;
+   return checkNumber(newIndex);
+  })
+ }
+
+ const giveRandom = ()=>{
+   let randomNumber = Math.floor(Math.random()*images.length);
+   if(randomNumber === index){
+    randomNumber =  randomNumber + 1;
+   }
+   setIndex(checkNumber(randomNumber))
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section>
+     <div key={id}>
+      <img src={image} alt={name} className='image'/>
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      </div>
+      <div className='btn-container'>
+       <button type='button' onClick={nextPerson}>Next</button>
+       <button type='button' onClick={prevPerson}>Prev</button>
+      </div>
+      <button type='button' onClick={giveRandom}>Random</button>
+     
+    </section>
+  )
 }
 
-export default App;
+export default App
---
+++ src/images.js
@@ -0,0 +1,8 @@
+export const images = [
+ {id:1,name:"John Thotho",job:"Website Developer",image:"https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=600"},
+ {id:2,name:"Peter Kamuren",job:"Photographer",image:"https://images.pexels.com/photos/769746/pexels-photo-769746.jpeg?auto=compress&cs=tinysrgb&w=600"},
+ {id:3,name:"Priscilla Njoki",job:"Engineer",image:"https://images.pexels.com/photos/948873/pexels-photo-948873.jpeg?auto=compress&cs=tinysrgb&w=600"},
+ {id:4,name:"Alex Verderbremen",job:"Teacher",image:"https://images.pexels.com/photos/1055691/pexels-photo-1055691.jpeg?auto=compress&cs=tinysrgb&w=600"},
+ {id:5,name:"John Kuffour",job:"Scientist",image:"https://images.pexels.com/photos/941710/pexels-photo-941710.jpeg?auto=compress&cs=tinysrgb&w=600"},
+ {id:6,name:"Sylvia Wangechi",job:"Digital Marketer",image:"https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=600"},
+]
